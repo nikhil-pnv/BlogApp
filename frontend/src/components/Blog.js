187c3a40cca13ecf5a84f45db6219b1be37e7d03
PINNAMARAJU NIKHIL VARMA <120675218+nikhil-pnv@users.noreply.github.com>
2023-10-07 12:56:44 +0530

    Update Blog.js

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -11,7 +11,7 @@ const Blog = ({ title, description, imageURL, userName, isUser, id }) => {
     };
     const deleteRequest = async() => {
         const res = await axios
-            .delete(`http://localhost:5000/api/blog/${id}`)
+            .delete(`https://blog-app-api-xi.vercel.app/api/blog/${id}`)
             .catch((err) => console.log(err));
         const data = await res.data;
         return data;
