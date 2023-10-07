10d480fea530040290abab5b392d4f218007ffcd
PINNAMARAJU NIKHIL VARMA <120675218+nikhil-pnv@users.noreply.github.com>
2023-10-07 12:54:21 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
         };
 
         const sendRequest = async(type = "login") => {
-            const res = await axios.post(`http://localhost:5000/api/user/${type}`, {
+            const res = await axios.post(`https://blog-app-api-xi.vercel.app/api/user/${type}`, {
                 name: inputs.name,
                 email: inputs.email,
                 password: inputs.password,
