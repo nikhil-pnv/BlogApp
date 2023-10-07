cfea6a782bf3b7883e0366156fbd2fb746417b5c
PINNAMARAJU NIKHIL VARMA <120675218+nikhil-pnv@users.noreply.github.com>
2023-10-07 12:57:22 +0530

    Update AddBlog.js

--- frontend/src/components/AddBlog.js
+++ frontend/src/components/AddBlog.js
@@ -20,7 +20,7 @@ const AddBlog = () => {
     };
 
     const sendRequest = async() => {
-        const res = await axios.post("http://localhost:5000/api/blog/add", {
+        const res = await axios.post("https://blog-app-api-xi.vercel.app/api/blog/add", {
             title: inputs.title,
             description: inputs.description,
             image: inputs.image,
