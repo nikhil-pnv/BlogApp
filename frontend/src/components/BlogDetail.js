9ff08512a1cf9b7c94235d894aa96a57633e5380
PINNAMARAJU NIKHIL VARMA <120675218+nikhil-pnv@users.noreply.github.com>
2023-10-07 12:56:15 +0530

    Update BlogDetail.js

--- frontend/src/components/BlogDetail.js
+++ frontend/src/components/BlogDetail.js
@@ -25,7 +25,7 @@ const BlogDetail = () => {
 
 
     const fetchDetails = async() => {
-        const res = await axios.get(`http://localhost:5000/api/blog/${id}`).catch((err) => console.log(err));
+        const res = await axios.get(`https://blog-app-api-xi.vercel.app/api/blog/${id}`).catch((err) => console.log(err));
         const data = await res.data;
         return data;
     }
