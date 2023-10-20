1ec48e2d6edd0543ef039bd2c1334dc89c7325b8
PINNAMARAJU NIKHIL VARMA <120675218+nikhil-pnv@users.noreply.github.com>
2023-10-20 10:56:39 +0530

    Update Blog.js

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -11,7 +11,7 @@ const Blog = ({ title, description, imageURL, userName, isUser, id }) => {
     };
     const deleteRequest = async() => {
         const res = await axios
-            .delete(`https://blog-app-api-phi.vercel.app/api/blog/${id}`)
+            .delete(`https://pnv-blog.onrender.com/api/blog/${id}`)
             .catch((err) => console.log(err));
         const data = await res.data;
         return data;
