8f0311c86706327328c1e5ebe1bc3faad4e5f67b
PINNAMARAJU NIKHIL VARMA <120675218+nikhil-pnv@users.noreply.github.com>
2023-10-20 10:59:37 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
         };
 
         const sendRequest = async(type = "login") => {
-            const res = await axios.post(`https://blog-app-api-phi.vercel.app/api/user/${type}`, {
+            const res = await axios.post(`https://pnv-blog.onrender.com/api/user/${type}`, {
                 name: inputs.name,
                 email: inputs.email,
                 password: inputs.password,
